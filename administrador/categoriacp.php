cf2f6cf66d8f57f906d9ac79570b03de202e58a9
arielleguizamon <ariel.leguizamon93@gmail.com>
2014-06-04 09:50:01 -0300

    agregado seguridad

--- administrador/categoriacp.php
+++ administrador/categoriacp.php
@@ -3,4 +3,44 @@ if (!($_COOKIE['isAdmin'] != '')) {
     echo 'error de permisos';
     die();
 }
+function addCategoria($nombre) {
+    include '../dbconnection.php';
+    $link = connectdb();
+    include '../queries.php';
+    q_addCategoria($nombre);
+    mysql_close($link);
+}
+
+function updateEditorial($id) {
+    include '../dbconnection.php';
+    $link = connectdb();
+    include '../queries.php';
+    q_updateCategoria($id);
+    mysql_close($link);
+}
+
+function delEditorial($id) {
+    include '../dbconnection.php';
+    $link = connectdb();
+    include '../queries.php';
+    q_removeCategoria($id);
+    mysql_close($link);
+}
+
+
+if (isset($_POST["inputDataEditorial"])) { // Para agregar
+    addCategoria($_POST['inputCategoria']);
+    header('Location: admincp.php');
+    exit();
+}
+elseif (isset($_POST["editDataCategoria"])) { // Para editar
+    updateEditorial($_POST['idCategoria'], $_POST['nombreCategoria']);
+    header('Location: listarCategoria.php');
+    exit();
+} 
+elseif (isset($_POST["deleteDataCategoria"])){ // para borrar
+        //$unNom= 'ana';
+        delEditorial($_POST["idCategoria"]); 
+        exit;
+}
 ?>
