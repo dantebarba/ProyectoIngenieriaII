5149e26d9d8c001f694e892e10e593d6f87628f7
arielleguizamon <ariel.leguizamon93@gmail.com>
2014-06-04 22:08:14 -0300

    funciona el add

--- administrador/autorcp.php
+++ administrador/autorcp.php
@@ -1,55 +1,23 @@
 <?php
+
 if (!($_COOKIE['isAdmin'] != '')) {
     echo 'error de permisos';
     die();
 }
-/* 
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
+/*
+  // NO PONER ID's IGUALES A DOS OBJETOS HTML
+
  */
 
 function addAutor($nombre) {
+    echo 'llego';
     include '../dbconnection.php';
     $link = connectdb();
     include '../queries.php';
     q_addAutor($nombre);
     mysql_close($link);
 }
-  
+
 function updateAutor($id, $nombre) {
     include '../dbconnection.php';
     $link = connectdb();
@@ -66,57 +34,42 @@ function delAutor($id) {
     mysql_close($link);
 }
 
+$element = $_POST["element"];
+echo $element;
+switch ($element) {
+    case 'autor_add': {
+        echo 'adentrodelCase';
+        addAutor($_POST['agregar_nombreAutor']);
+        header('Location: admincp.php');
+        break;
+        }
+    case 'autor_edit': {
+        updateAutor($_POST['editar_idAutor'], $_POST['editar_nombreAutor']);
+        header('Location: listarAutor.php');
+        }
+    case 'autor_del': {
+        delAutor($_POST['eliminar_idAutor']);
+        header('Location: listarAutor.php');
+        }
+}
+exit();
 //TODO: POR QUE CARAJO CUANDO LLAMO AL FORM NO ME FIGURA ISSET, PERO CUANDO
 // LLAMO AL CAMPO SI? ES POR JAVASCRIPT? TENGO QUE TIRARLE UN TRIGGER?
-if (isset($_POST["inputDataAutor"])) {
-    addAutor($_POST['agregar_nombreAutor']);
-    header('Location: admincp.php');
-    exit();
-}
-elseif (isset($_POST["editar_idAutor"])) {
-    updateAutor($_POST['editar_idAutor'], $_POST['editar_nombreAutor']);
-    header('Location: listarAutor.php');
-    exit();
-} elseif (isset($_POST["eliminar_idAutor"])){ // button name
-    delAutor($_POST['eliminar_idAutor']); 
-    header('Location: listarAutor.php');
-    exit();
-}
+//if (isset($_POST["inputDataAutor"])) {
+  //  addAutor($_POST['agregar_nombreAutor']);
+    //header('Location: admincp.php');
+//    exit();
+//} elseif (isset($_POST["editar_idAutor"])) {
+//    updateAutor($_POST['editar_idAutor'], $_POST['editar_nombreAutor']);
+//    header('Location: listarAutor.php');
+//    exit();
+//} elseif (isset($_POST["eliminar_idAutor"])) { // button name
+//    delAutor($_POST['eliminar_idAutor']);
+//    header('Location: listarAutor.php');
+//    exit();
+//}
 
 // NO PONER ID's IGUALES A DOS OBJETOS HTML
 // NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
-// NO PONER ID's IGUALES A DOS OBJETOS HTML
+
 
