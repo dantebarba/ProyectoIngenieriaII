511be9db68b67dbe26132696505ac608fdaf6193
DANTE <dantebarba@gmail.com>
2014-06-28 00:09:42 -0300

    fixed != vs !==

--- restricted/securitycheck.php
+++ restricted/securitycheck.php
@@ -1,14 +1,14 @@
 <?php
 
 function adminCheck() {
-    if (isset($_COOKIE) and $_COOKIE['isAdmin'] !== 0) {
+    if (isset($_COOKIE) and $_COOKIE['isAdmin'] != 0) {
         return true;
     }
     else { return false; }
 }
 
 function loginCheck() {
-    if (isset($_COOKIE) and $_COOKIE['username'] !== '') {
+    if (isset($_COOKIE) and $_COOKIE['username'] != '') {
         return true;
     }
     else {return false;} 
