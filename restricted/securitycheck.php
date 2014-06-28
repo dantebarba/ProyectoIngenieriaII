7ce3b0808435df4d96160db025c955d1921e2f84
DANTE <dantebarba@gmail.com>
2014-06-28 00:21:27 -0300

    -solucionado definitivamente

--- restricted/securitycheck.php
+++ restricted/securitycheck.php
@@ -1,14 +1,14 @@
 <?php
 
 function adminCheck() {
-    if (isset($_COOKIE) && $_COOKIE['isAdmin'] != 0) {
+    if (isset($_COOKIE['isAdmin']) && $_COOKIE['isAdmin'] != 0) {
         return true;
     }
     else { return false; }
 }
 
 function loginCheck() {
-    if (isset($_COOKIE) && $_COOKIE['username'] != '') {
+    if (isset($_COOKIE['username']) && $_COOKIE['username'] != '') {
         return true;
     }
     else {return false;} 
