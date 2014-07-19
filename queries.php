2a44535d60c6903a4b0617078e2e914a107343d5
DANTE <dantebarba@gmail.com>
2014-07-19 02:24:36 -0300

    -update de libros

--- queries.php
+++ queries.php
@@ -149,6 +149,22 @@ function q_updateLibro($dataCollection) {
             . "`descripcion`='".$dataCollection['descripcion']."'"
             . " WHERE ".$dataCollection['oldISBN']."=ISBN";
     mysql_query($query) or die(mysql_error());
+    $query = 
+            "UPDATE 'compras_has_libros' SET"
+            . "Libros_ISBN=".$dataCollection["ISBN"]."WHERE ".$dataCollection["oldISBN"]." = ISBN";
+    mysql_query($query) or die(mysql_error());
+    $query = 
+            "UPDATE 'etiquetas_has_libros' SET"
+            . "Libros_ISBN=".$dataCollection["ISBN"]."WHERE ".$dataCollection["oldISBN"]." = ISBN";
+    mysql_query($query) or die(mysql_error());
+    $query = 
+            "UPDATE 'libros_has_autores' SET"
+            . "Libros_ISBN=".$dataCollection["ISBN"]."WHERE ".$dataCollection["oldISBN"]." = ISBN";
+    mysql_query($query) or die(mysql_error());
+    $query = 
+            "UPDATE 'compras_has_editoriales' SET"
+            . "Libros_ISBN=".$dataCollection["ISBN"]."WHERE ".$dataCollection["oldISBN"]." = ISBN";
+    mysql_query($query) or die(mysql_error());
 }
 
 function q_updateCompra($dataCollection) {
